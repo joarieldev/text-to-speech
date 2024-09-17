075ce8137d23ea6c77b7829ff71c3ce29127974b
Jose Martinez <joariel17@gmail.com>
2024-09-16 21:41:50 -0300

    seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,18 @@
-// @ts-check
+// @ts-nocheck
 import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
+import astroCompress from 'astro-compress';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [
+    tailwind(),
+    astroCompress({
+      gzip: true,
+      brotli: true,
+      minify: true
+    }),
+  ]
 });
